94de5eba3f6fe364e8fd0f2dcbaa40381bb10c3c
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-09-13 20:41:26 -0300

    Creating our first mitration
    Here i created my first migration and connected our db to MS SQL SERVER

---
+++ program.cs
@@ -0,0 +1,46 @@
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddTransient<Seeder>();
+builder.Services.AddDbContext<TodoContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); //Here we reference the conection string to the one in appsetings.json
+//builder.Services.AddSwaggerGen(c =>
+//{
+//    c.SwaggerDoc("v1", new() { Title = "TodoApi", Version = "v1" });
+//});
+
+var app = builder.Build();
+ 
+if (args.Length == 1 && args[0].ToLower() == "seeddata")
+SeedData(app);
+
+void SeedData(IHost app){
+    var scopedFactory =app.Services.GetService<IServiceScopeFactory>();
+
+    using(var scope = scopedFactory.CreateScope ()){
+        var service = scope.ServiceProvider.GetService<Seeder>();
+        service.Seed();
+    }
+}
+
+// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    //app.UseSwagger();
+    //app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
