062028bbfb6e8eca26ee9be420b27c43ee1a8062
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-03 21:53:29 -0300

    Create user.cs

---
+++ user.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class User
+    {
+        public int Id {get; set;}
+        public int UserId { get; set; }
+        public string Name { get; set; }
+
+        public List<TodoItem> TodoItems { get; set; }
+    }
+}
