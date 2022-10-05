db9a944f57ab641bae1e71a3a3d88cc5a62440a3
David Aceituno <77316307+DavidAceituno88@users.noreply.github.com>
2022-10-04 22:43:53 -0300

    Update todoContext.cs
    Seeding some data

--- todoContext.cs
+++ todoContext.cs
@@ -10,6 +10,35 @@ namespace TodoApi.Models
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<User>().HasData(
+                new User 
+                {
+                    Id = 1,
+                    UserId = 10,
+                    Name = "Omar Ramo"
+                },
+                new User 
+                {
+                    Id = 2,
+                    UserId = 11,
+                    Name = "Ana"
+                },
+                new User 
+                {
+                    Id = 3,
+                    UserId = 12,
+                    Name = "Tonathiu"
+                },
+                new User 
+                {
+                    Id = 4,
+                    UserId = 13,
+                    Name = "Hirales"
+                }
+            );
+        }
+
         public DbSet<TodoItem> TodoItems { get; set; }
         public DbSet<User> Users {get; set;}
        
